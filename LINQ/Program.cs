acd0106bbf0ab5eaa91d8665ee07a8824c59d545
ZanyaMyers <myerszanya@gmail.com>
2023-11-21 11:58:43 -0800

    inital commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGames = new List<string>() { "Halo 3" , "CODMW2" , "Left For Dead"};
+            videoGames.Add("OSRS");
+            videoGames.Add("2K16");
+
+            var orderedGames = videoGames.OrderBy(name => name.Length); 
+
+            foreach (var game in orderedGames)
+            {
+                Console.WriteLine("List of Games:");
+                Console.WriteLine(game);
+            }
         }
     }
 }
